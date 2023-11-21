b67bced26463a529630362d734ea26d6be286984
tzuminglu <1994515daniel@gmail.com>
2023-11-21 16:34:46 -0500

    update firebase Auth

---
+++ react-client/src/firebase/FirebaseConfig.js
@@ -0,0 +1,11 @@
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+
+export default {
+  apiKey: "AIzaSyBErgsKWExB5I_k-bfa88wrFWxoyjY8Mq4",
+  authDomain: "cs554-groupvviv.firebaseapp.com",
+  projectId: "cs554-groupvviv",
+  storageBucket: "cs554-groupvviv.appspot.com",
+  messagingSenderId: "497446602309",
+  appId: "1:497446602309:web:1a320f49c43d1a663b452c"
+};
--- react-client/src/main.jsx
+++ react-client/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import fbconfig from "./firebase/FirebaseConfig";
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp(fbconfig);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
