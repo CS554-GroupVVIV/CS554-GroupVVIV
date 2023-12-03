41a7cf8ddc52184cca32750db037662828039d09
Chia-Hsiang Wu <cwu34@stevens.edu>
2023-12-03 15:13:04 -0500

    backend package and files

---
+++ back-end/data/index.js
@@ -0,0 +1,7 @@
+import * as users from "./users.js";
+import * as products from "./products.js";
+import * as posts from "./posts.js";
+
+export const userData = users;
+export const productData = products;
+export const postData = posts;
