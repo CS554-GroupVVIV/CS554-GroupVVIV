104bc1605706333dc99b37f5d601cd9ad787261d
tzuminglu <1994515daniel@gmail.com>
2023-12-05 16:01:27 -0500

    update firebase

---
+++ front-end/sit-marketplace/src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,14 @@
+export default {
+  //   apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  //   authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+  //   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  //   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  //   messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  //   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBErgsKWExB5I_k-bfa88wrFWxoyjY8Mq4",
+  authDomain: "cs554-groupvviv.firebaseapp.com",
+  projectId: "cs554-groupvviv",
+  storageBucket: "cs554-groupvviv.appspot.com",
+  messagingSenderId: "497446602309",
+  appId: "1:497446602309:web:1a320f49c43d1a663b452c",
+};
