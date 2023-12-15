da5d8244bc876350b87cf40c0c75ec1c6de83502
aturret <enturreopy@gmail.com>
2023-12-15 15:41:11 -0500

    Add env vars for back-end services

--- front-end/sit-marketplace/src/components/socket.ts
+++ front-end/sit-marketplace/src/components/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4001");
+export const socket = io(process.env.SOCKETIO_URL || "http://localhost:4001");
 export let socketID = "";
 socket.on("connect", () => {
   socketID = socket.id;
