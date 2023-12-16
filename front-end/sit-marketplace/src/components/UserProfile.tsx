90c7606fec4f1776d90fdbe94907c112f703ebd2
tzuminglu <1994515daniel@gmail.com>
2023-12-16 18:52:05 -0500

    update userprofile

--- front-end/sit-marketplace/src/components/UserProfile.tsx
+++ front-end/sit-marketplace/src/components/UserProfile.tsx
@@ -34,6 +34,7 @@ function UserProfile() {
   const client = useApolloClient();
   const { loading, error, data } = useQuery(GET_USER, {
     variables: { id: currentUser ? currentUser.uid : "" },
+    fetchPolicy: "cache-and-network",
   });
   console.log(data);
   const [userInfo, setUserInfo] = useState(null);
