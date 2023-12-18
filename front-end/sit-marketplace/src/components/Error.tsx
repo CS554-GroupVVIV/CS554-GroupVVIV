41dbf89683905fc19a1d906249c977c2619bde29
Daixuan Chen <574124970@qq.com>
2023-12-17 20:56:08 -0500

    modify error page

--- front-end/sit-marketplace/src/components/Error.tsx
+++ front-end/sit-marketplace/src/components/Error.tsx
@@ -1,12 +1,30 @@
-import { Card, Container, CardContent, Grid, Link } from "@mui/material";
-import { useNavigate } from "react-router";
+import { Container, Link } from "@mui/material";
+import { useNavigate, useLocation } from "react-router";
 
-export default function Error({ messgae }) {
+export default function Error() {
+  const location = useLocation();
   const navigate = useNavigate();
+  const { message, statusCode } = location.state;
+  console.log("message", message);
+  console.log("statusCode", statusCode);
+  let errorMessage;
+  switch (statusCode) {
+    case 400:
+      errorMessage = "Bad Request";
+      break;
+    case 404:
+      errorMessage = `${message} not found`;
+      break;
+    default:
+      errorMessage = message;
+      break;
+  }
+
   return (
     <div>
       <Container maxWidth="lg">
-        {messgae && <p>Error:{message}</p>}
+        <h1>Error {statusCode}</h1>
+        <p>{errorMessage}</p>
         <Link onClick={() => navigate("/")}>
           <h1>Home</h1>
         </Link>
