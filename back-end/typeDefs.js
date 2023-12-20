0c21671ea11239c626ba4c1f5d3ca087d132e98c
Daixuan Chen <574124970@qq.com>
2023-12-20 18:43:54 -0500

    fix bugs in add user

--- back-end/typeDefs.js
+++ back-end/typeDefs.js
@@ -99,7 +99,7 @@ export const typeDefs = `#graphql
         addPossibleSeller(_id:String!,seller_id:String!):Post,
         editPost(_id: String!, buyer_id: String!,seller_id:String, item:String!, category:String!, price: Float!, condition:String!, description:String, status:String!):Post,
         removePost(_id:String!):Post,
-        addUser(_id: String!, email: String!, firstname: String!, lastname: String!, favorite: String,favorite_post:String!): User,
+        addUser(_id: String!, email: String!, firstname: String!, lastname: String!, favorite: String): User,
         editUser(_id: String!, email: String!, firstname: String!, lastname: String!): User,
         addChat(participants: [String!]!): Chat,
         addMessage(_id: String!, sender: ID!, time: DateTime!, message: String!): Message,
