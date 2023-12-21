7f957c0cb9ab163725f9fd9f985ee9987bfad6be
aturret <enturreopy@gmail.com>
2023-12-21 14:42:26 -0500

    update

--- front-end/sit-marketplace/src/aws.jsx
+++ front-end/sit-marketplace/src/aws.jsx
@@ -6,13 +6,17 @@ AWS.config.update({
   region: import.meta.env.VITE_AWS_REGION || "us-west-1",
 });
 
+// const s3 = new AWS.S3({
+//   apiVersion: "2006-03-01",
+//   signatureVersion: "v4",
+// });
 const s3 = new AWS.S3();
 
 export const uploadFileToS3 = async (file, name) => {
   const timestamp = Date.now().toString();
   const params = {
     Bucket: import.meta.env.VITE_AWS_S3_BUCKET_NAME,
-    Key: name + timestamp + file.name,
+    Key: name + timestamp + encodeURIComponent(file.name),
     Body: file,
   };
 
